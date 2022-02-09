fc0eea54f67f4c654a9b639ec2c194096b080da5
andrevillage <99328594+andrevillage@users.noreply.github.com>
2022-02-09 13:56:12 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,39 @@
-from pyrogram import Client, Filters
+import os
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+from translation import Translation
+from pyrogram import Client, filters
+from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant
 
-
-@Client.on_message(Filters.command(["help"]))
-async def start(client, message):
-    helptxt = f"**Merhaba**\n\nBen Bir YouTube Video ve Ses İndirme Botuyum. Beni Kullanmak İçin Bana Bir YouTube URL Gönder VEYA @vid Yazarak İstediğin Videoyu Arayarak Bulabilirisin. Unutma Playlist Linklerini Desteklemiyorum.\n\n**Geliştirici**: @trbotlistesi\n**Yardım**: @trbotlistesidestek"
-    await message.reply_text(helptxt)
+@Client.on_message(filters.private & filters.command(["start", "help"]))
+async def start(bot, update):
+    update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "banned":
+               await bot.delete_messages(
+                 chat_id=update.chat.id,
+                 message_ids=update.message_id,
+                 revoke=True
+               )
+               return
+        except UserNotParticipant:
+            await update.reply_text(
+                text="**Botu sadece kanal aboneleri kullanabilir.**",
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton(text="Kanala Katil", url=f"https://t.me/{update_channel}")]
+                ])
+            )
+            return
+        except Exception:
+            await update.reply_text("Ters giden bir şey mi var. @thebans ile iletişime geçin")
+            return
+        await update.reply_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=Translation.START_BUTTONS
+           )
